f0b0a09d0415e79ae1f77da852b7e1468433d2bc
qwertyyb <qwertyyb@foxmail.com>
2025-12-16 18:48:20 +0800

    feat: 简化页面

--- .vitepress/theme/DocsList.vue
+++ .vitepress/theme/DocsList.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="docs-list">
-    <img src="https://cn.bing.com/th?id=OHR.CopanRuins_EN-US5517813382_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4" alt="" class="cover">
     <div class="content">
       <ul class="list">
         <li class="item" v-for="(item, index) in docs" :key="index">
@@ -24,11 +23,6 @@ const formatDate = (date: string) => {
 </script>
 
 <style>
-.docs-list .cover {
-  width: 100%;
-  height: 50vh;
-  object-fit: cover;
-}
 .docs-list .content {
   max-width: calc(var(--vp-layout-max-width) - 64px);
   padding: 16px;
@@ -41,7 +35,7 @@ const formatDate = (date: string) => {
   padding: 12px 0;
 }
 .docs-list .item + .item {
-  border-top: 1px solid #bbb;
+  border-top: 1px solid var(--vp-c-gutter);
 }
 .docs-list .item {
   transition: all .2s;
@@ -50,7 +44,7 @@ const formatDate = (date: string) => {
   color: var(--vp-c-brand-1);
 }
 .docs-list .doc-title {
-  font-size: 22px;
+  font-size: 16px;
   margin: 0 0 4px;
   font-weight: 400;
 }
